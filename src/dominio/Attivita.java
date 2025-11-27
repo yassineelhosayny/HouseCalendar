9535bfc6f237c0fe24a388ca72b3e3d1506b512d
darkPheonix <yassineelhosayny99@gmail.com>
2025-11-27 21:23:53 +0100

    add variabile e costruttore di class Attivita

--- src/dominio/Attivita.java
+++ src/dominio/Attivita.java
@@ -1,12 +1,30 @@
 package dominio;
 
+import java.time.LocalDate;
+
 public abstract class Attivita {
     
     //attributi essenziali per una attivita PROTECTED
+    protected String descrizione;
+    protected TipoAttivita tipo;
+    protected LocalDate dataScadenza;
+    protected LocalDate dataNotifica;
+    protected int priorita;
+    protected boolean completato;
+    protected Utente utenteAssegnato;
+    protected boolean attivitaPrivata; //visibile solo all'utente assegnato
 
     //costrutorre generale per Attivita
-    public Attivita(/*attribute essenziale per una attivita */) {
-        //TODO:
+    public Attivita(String descrizione, TipoAttivita tipo, LocalDate dataScadenza,
+                    LocalDate dataNotifica, int priorita, Utente utenteAssegnato, boolean attivitaPrivata) {
+        this.descrizione = descrizione;
+        this.tipo = tipo;
+        this.dataScadenza = dataScadenza;
+        this.dataNotifica = dataNotifica;
+        this.priorita = priorita;
+        this.completato = false; //inizialmente non completato
+        this.utenteAssegnato = utenteAssegnato;
+        this.attivitaPrivata = attivitaPrivata;
     }
     //METODI PER ATTIVITA
     //1/ GETER E SETTER per attributi essenziali
